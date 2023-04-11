5456d8b66f829b2cd6bbf80333f7625aad179830
Leonardo Henrique <99227509+Sazakii@users.noreply.github.com>
2023-04-11 00:11:58 -0300

    Finalização main

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ let toggleBtn = document.querySelector('.toggle_btn')
 let toggleBtnIcon = document.querySelector('.toggle_btn i')
 let dropDownMenu = document.querySelector('.dropdown_menu')
 
-toggleBtn.onclick = function (){
+toggleBtn.onclick = function() {
     dropDownMenu.classList.toggle('open')
     let isOpen = dropDownMenu.classList.contains('open')
 
